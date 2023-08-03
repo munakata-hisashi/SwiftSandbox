c4c2118655f347cab3664f25e753b8f1965ea1be
hisashi.munakata <hisashi.munakata@septeni-original.co.jp>
2023-08-03 20:39:31 +0900

    hoge

---
+++ Sources/SwiftSandbox/AsyncSequenceExample.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by h_munakata on 2023/03/08.
+//
+
+import Foundation
--- Sources/SwiftSandbox/SwiftSandbox.swift
+++ Sources/SwiftSandbox/SwiftSandbox.swift
@@ -3,8 +3,10 @@ public struct SwiftSandbox {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
-        let lensRectangle = Lens(value: Rectangle(topLeft: Point(x: 10, y: 10), bottomRight: Point(x: 20, y: 20)))
-        print(lensRectangle.bottomRight) // Point(x: 20, y: 20) LensのプロパティかのようにPointのtopLeftプロパティにアクセス
-        print(lensRectangle[keyPath: \.bottomRight])
+        
+        
+        let a = A()
+        
+        print(a is Sendable)
     }
 }
