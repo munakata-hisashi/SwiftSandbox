eb21f849ab4b3513763b4f1a40a794847f496d4e
hisashi.munakata <hisashi.munakata@septeni-original.co.jp>
2024-08-21 11:21:38 +0900

    async

--- Sources/SwiftSandbox/SwiftSandbox.swift
+++ Sources/SwiftSandbox/SwiftSandbox.swift
@@ -2,7 +2,7 @@
 public struct SwiftSandbox {
     public private(set) var text = "Hello, World!"
 
-    public static func main() {
+    public static func main() async throws {
         let array = [1, 2, 3]
         print(array.description)
         let users = [User(name: "hoge"), User(name: "fuga")]
